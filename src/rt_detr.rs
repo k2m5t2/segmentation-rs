7410e0a2a56ead5b06fb82a8e9bcb0a216dfa991
Yunho Cho <yhjc2692@gmail.com>
2024-03-14 00:40:57 -0400

    Progress MVP (60%)

--- src/rt_detr.rs
+++ src/rt_detr.rs
@@ -19,11 +19,14 @@ use image::imageops::FilterType;
 // use imageproc::rect::Rect;
 // machine learning
 use ort::{GraphOptimizationLevel, ModelMetadata, Session};
+// graphics
+use tiny_skia::*;
 
 use ndarray_stats::QuantileExt;
 
 use serde_json::{Value, Map};
 use std::collections::HashMap;
+
 
 // mod utils2;
 use crate::utils2::{image_to_onnx_input, crop_square};
@@ -38,11 +41,19 @@ fn center_coords_to_box_coords(cx: i32, cy: i32, w: i32, h: i32) -> (i32, i32, i
 
     return (x_left, x_right, y_top, y_bottom);
 }
+fn center_coords_to_box_coords_f32(cx: f32, cy: f32, w: f32, h: f32) -> (f32, f32, f32, f32) {
+    let x_left = cx - w / 2.;
+    let x_right = cx + w / 2.;
+    let y_top = cy - h / 2.;
+    let y_bottom = cy + h / 2.;
+
+    return (x_left, x_right, y_top, y_bottom);
+}
 
 
 // fn thresholded_argmax(a: Array<f32, Dim<IxDynImpl>>, threshold: f32) -> Array1<u32> { // takes argmax of rows above threshold // https://stackoverflow.com/a/57963733/15275714
 // fn thresholded_argmax(a: Array<f32, IxDyn>, threshold: f32) -> Array1<u32> { // takes argmax of rows above threshold // https://stackoverflow.com/a/57963733/15275714
-fn thresholded_argmax(a: Array<f32, Ix2>, threshold: f32) -> Array1<u32> { // takes argmax of rows above threshold // https://stackoverflow.com/a/57963733/15275714
+fn thresholded_argmax_2d(a: Array<f32, Ix2>, threshold: f32) -> Array1<u32> { // takes argmax of rows above threshold // https://stackoverflow.com/a/57963733/15275714
     let mut indices = Array1::zeros(a.shape()[0]);
     for (i, row) in a.axis_iter(Axis(0)).enumerate() {
         let (max_idx, max_val) =
@@ -55,11 +66,16 @@ fn thresholded_argmax(a: Array<f32, Ix2>, threshold: f32) -> Array1<u32> { // ta
                         (idx, *val)
                     }
                 });
-            // println!("max idx for row {}: {}", i, max_idx); // DEBUG
-            if max_val > threshold { indices[i] = max_idx as u32; }
-            else { indices[i] = 0; }
+        // println!("max idx for row {}: {}", i, max_idx); // DEBUG
+        if max_val > threshold { indices[i] = max_idx as u32; }
+        else { indices[i] = 0; }
     }
     return indices;
+}
+
+fn argmax(a: Array1<f32>) -> usize {
+    let (max_idx, max_val) = a.iter().enumerate().fold((0 as usize, 0.), |(max_idx, max_val), (cur_idx, &cur_val)| if max_val < cur_val { (cur_idx, cur_val) } else { (max_idx, max_val) } );
+    return max_idx;
 }
 
 fn thresholded_mask(a: Array<f32, Ix2>, threshold: f32) -> Array1<bool> { // takes argmax of rows above threshold // https://stackoverflow.com/a/57963733/15275714
@@ -186,6 +202,7 @@ impl RtDetr {
     pub fn process(self) -> Result<(), Box<dyn Error>> {
         // load image
         let mut original_image = ImageReader::open("./test_images/obj_det/computer_desk.jpg")?.decode()?;
+        // let mut original_image = ImageReader::open("./test_images/obj_det/women_umbrella.jpg")?.decode()?;
         let original_image_size = original_image.dimensions();
         
         // resize image
@@ -201,12 +218,12 @@ impl RtDetr {
         // run it thru model
         let ort_outputs = self.model.run(ort_inputs)?; // NOTE if image size is not right, this crashes silently (without panicking).
         let outputs = ort_outputs["output0"].extract_tensor::<f32>()?;
-        let outputs_view = outputs.view().clone().into_owned();
+        let batch_outputs_view = outputs.view().clone().into_owned();
         
-        let output_view = outputs_view.slice(s![0,..,..]).view().clone().into_owned();
+        let outputs_view = batch_outputs_view.slice(s![0,..,..]).view().clone().into_owned(); // choose only the first image (temp)
         
         // post-process predictions
-        println!("{:?}", output_view);
+        // println!("{:?}", outputs_view);
         // println!("{:?}", output0_view.slice(s![0, 0, ..]));
 
         // label
@@ -214,7 +231,7 @@ impl RtDetr {
         let class_dict: Value = serde_json::from_str(&class_dict_raw)?;
         let class_list_raw = self.metadata.custom("names_json_array").unwrap().unwrap();
         let class_list: Value = serde_json::from_str(&class_list_raw)?; // not bad!
-        println!("{:?}", class_list);
+        // println!("{:?}", class_list);
 
         // process preds into boxes: filter confidences, get coordinates, run NMS
         // NOTE still want to understand how it works (although this isn't strictly relevant/important): read YOLACT's paper and see what it says, and ask ChatGPT. 
@@ -222,31 +239,46 @@ impl RtDetr {
         // filter confidences
         // NOT not sure about the code quality / effectiveness here. perhaps refactor depending on what code reviews/GPT/readability/efficiency/reusability says on it.
         // let confident = output0_view.slice(s![..,4..]).max(Axis(1)) > 0.9;
-        let confident = thresholded_mask(output_view.slice(s![..,4..]).clone().into_owned(), self.threshold);
-        let confident_indices = thresholded_argmax(output_view.slice(s![..,4..]).clone().into_owned(), self.threshold);
+        let confident = thresholded_mask(outputs_view.slice(s![..,4..]).clone().into_owned(), self.threshold);
+        let confident_indices = thresholded_argmax_2d(outputs_view.slice(s![..,4..]).clone().into_owned(), self.threshold);
         // println!("{:?}", confident);
         
-        let confident_output_view = apply_mask(&output_view, &confident);
-        println!("{:?}", confident_output_view);
+        let confident_outputs_view = apply_mask(&outputs_view, &confident);
+        println!("{:?}", confident_outputs_view);
 
-        let relevant_output = apply_mask(&output_view, &confident);
+        let relevant_outputs: ArrayBase<OwnedRepr<f32>, Dim<[usize; 2]>> = apply_mask(&outputs_view, &confident);
 
-        for 
+        let mut path_builder = PathBuilder::new();
 
+        for (i, box_pred) in relevant_outputs.axis_iter(Axis(0)).enumerate() {
 
+            let (r_cx, r_cy, r_w, r_h) = (box_pred[0], box_pred[1], box_pred[2], box_pred[3]); // relative
+            // u32
+            // let (i_cx, i_cy, i_w, i_h) = ((r_cx * image_size.0 as f32) as u32, (r_cy * image_size.1 as f32) as u32, (r_w * image_size.0 as f32) as u32, (r_h * image_size.1 as f32) as u32); // input image coordinates
+            // let (o_cx, o_cy, o_w, o_h) = ((r_cx * original_image_size.0 as f32) as u32, (r_cy * original_image_size.1 as f32) as u32, (r_w * original_image_size.0 as f32) as u32, (r_h * original_image_size.1 as f32) as u32); // original image coordinates
+            // i32
+            // let (i_cx, i_cy, i_w, i_h) = ((r_cx * image_size.0 as f32) as i32, (r_cy * image_size.1 as f32) as i32, (r_w * image_size.0 as f32) as i32, (r_h * image_size.1 as f32) as i32); // input image coordinates
+            // let (o_cx, o_cy, o_w, o_h) = ((r_cx * original_image_size.0 as f32) as i32, (r_cy * original_image_size.1 as f32) as i32, (r_w * original_image_size.0 as f32) as i32, (r_h * original_image_size.1 as f32) as i32); // original image coordinates
+            // f32
+            let (i_cx, i_cy, i_w, i_h) = (r_cx * image_size.0 as f32, r_cy * image_size.1 as f32, r_w * image_size.0 as f32, r_h * image_size.1 as f32); // input image coordinates
+            let (o_cx, o_cy, o_w, o_h) = (r_cx * original_image_size.0 as f32, r_cy * original_image_size.1 as f32, r_w * original_image_size.0 as f32, r_h * original_image_size.1 as f32); // original image coordinates
+            // TODO readability.
+
+            let (i_x_l, i_x_r, i_y_t, i_y_b) = center_coords_to_box_coords_f32(i_cx, i_cy, i_w, i_h);
+            
+            let label = class_list[argmax(box_pred.slice(s![4..]).clone().into_owned())].as_str().unwrap();
+            println!("{:?}", (label)); // DEBUG; EVAL
+
+            path_builder.push_rect(Rect::from_ltrb(i_x_l , i_y_t, i_x_r, i_y_b).unwrap());
+
+        }
+
+        // println!("{:?}", ()); // DEBUG; EVAL
         
-        println!("{:?}", ()); // DEBUG; EVAL
 
         // for box_pred in output_view.axis_iter(Axis(0)) {
         //     println!("box_pred: {:?}", box_pred); // DEBUG
-
-        //     let (r_cx, r_cy, r_w, r_h) = (box_pred[0], box_pred[1], box_pred[2], box_pred[3]); // relative
-        //     let (i_cx, i_cy, i_w, i_h) = (r_cx * image_size[0], r_cy * image_size[1], r_w * image_size[0], r_h * image_size[1]); // input image coordinates
-        //     let (o_cx, o_cy, o_w, o_h) = (r_cx * image_size[0], r_cy * image_size[1], r_w * image_size[0], r_h * image_size[1]); // original image coordinates
-
-        //     let (x_l, x_r, y_t, y_b) = center_coords_to_box_coords(cx, cy, w, h);
-                
-        //     let indices = thresholded_argmax(box_pred.into_owned(), 0.90);
+        //     let indices = thresholded_argmax_2d(box_pred.into_owned(), 0.90);
         //     let nonzero_indices = drop_zeros(indices);
         //     // println!("box_pred (plus 1): {:?}", nonzero_indices.clone() + 1); // DEBUG
 
@@ -265,6 +297,26 @@ impl RtDetr {
         // create masks & visualize
         
         // skia: rectangle, color, text(?! may not support. bitmap texting is fine, as long as safe)
+
+        let img_rgba = image.clone().to_rgba8(); 
+        let mut pixmap = Pixmap::from_vec( // NOTE compiler warns
+          img_rgba.into_raw(), // This converts the DynamicImage into a Vec<u8> of raw pixels
+          IntSize::from_wh(image_size.0 as u32, image_size.1 as u32).unwrap()
+        ).expect("Failed to create Pixmap");
+
+        let paint = Paint {
+            shader: Shader::SolidColor(Color::from_rgba8(180, 229, 255, 255)), // Green color
+            anti_alias: true,
+            ..Default::default()
+          };
+
+        let path = path_builder.finish().unwrap();
+
+        // pixmap.fill_path(&path, &paint, FillRule::Winding, Transform::identity(), None);
+        let stroke = Stroke::default();
+        pixmap.stroke_path(&path, &paint, &stroke, Transform::identity(), None);
+
+        pixmap.save_png("viz.png").unwrap();
 
         // process preds into masks: matrix manipulation (transpose, flatten), multiply by weights, reshape/un-flatten, threshold, convert to binary, crop, combine layers, create translucent mask visualizations
 
